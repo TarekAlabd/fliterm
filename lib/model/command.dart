e1190a9ed44df2b0a83f05275167bf121d05c176
Ahmed Aboelyazeed <ahmdaeyz@gmail.com>
2020-12-25 19:02:27 +0200

    Implement ShellComponent for better modelling of the shell
    Signed-off-by: ahmdaeyz <ahmdaeyz@gmail.com>

---
+++ lib/model/command.dart
@@ -0,0 +1,8 @@
+import 'package:fliterm/model/shell_component.dart';
+import 'package:flutter/foundation.dart';
+
+class Command extends ShellComponent {
+  final String text;
+
+  Command({@required this.text});
+}
---
+++ lib/model/output.dart
@@ -0,0 +1,8 @@
+import 'package:fliterm/model/shell_component.dart';
+import 'package:flutter/foundation.dart';
+
+class Output extends ShellComponent {
+  final String text;
+
+  Output({@required this.text});
+}
