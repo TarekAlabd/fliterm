0a47628f96bf43689190efdda9b8bb34c42b4d50
Ahmed Aboelyazeed <ahmdaeyz@gmail.com>
2020-12-25 19:03:31 +0200

    Break down and format code and add automatic scroll on command submission
    Signed-off-by: ahmdaeyz <ahmdaeyz@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'dart:convert';
-import "package:path/path.dart" as Path;
 import 'dart:io';
+
+import 'package:fliterm/model/command.dart';
+import 'package:fliterm/model/output.dart';
+import 'package:fliterm/model/shell_component.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -47,7 +49,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController controller = TextEditingController();
-  List<String> lines = [];
+  List<ShellComponent> lines = [];
+  final _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -59,19 +62,15 @@ class _HomePageState extends State<HomePage> {
           children: [
             Expanded(
               child: SingleChildScrollView(
+                controller: _scrollController,
                 child: Container(
                   child: Align(
                     alignment: Alignment.topLeft,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: lines
-                          .map((e) => Text(
-                                '~\$ $e',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ))
-                          .toList(),
+                      children: lines.map((shellComponent) {
+                        return buildShellComponent(shellComponent);
+                      }).toList(),
                     ),
                   ),
                 ),
@@ -82,71 +81,22 @@ class _HomePageState extends State<HomePage> {
                 Text(
                   '~\$ ',
                   style: TextStyle(
-                    color: Colors.white,
+                    color: Colors.green,
                   ),
                 ),
                 Expanded(
                   child: TextFormField(
                     controller: controller,
                     decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                      )
-                    ),
+                        border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                    )),
                     style: TextStyle(
                       color: Colors.white,
                     ),
                     autovalidateMode: AutovalidateMode.onUserInteraction,
                     onEditingComplete: () async {
-                      // print('Enteeeeeeeeeeeeer');
-                      setState(() {
-                        lines.add(controller.text);
-                      });
-                      final process =
-                          await Process.run(controller.text.toLowerCase(), [])
-                              .catchError((error) => print(error));
-                      final envVarMap = Platform.environment;
-                      print(process?.stdout);
-                      if (process != null ||
-                          envVarMap[controller.text.toUpperCase()] != null)
-                        setState(() {
-                          lines.add(process?.stdout ??
-                              envVarMap[controller.text.toUpperCase()]);
-                        });
-                      else {
-                        List<String> wordsOfTheCommand =
-                            controller.text.split(' ');
-                        String firstWord = wordsOfTheCommand[0];
-                        if (firstWord == 'cat') {
-                          var theContentsOfTheFile =
-                              await _readAFile(wordsOfTheCommand[1] ?? '');
-                          // print('cat heeere');
-                          setState(() {
-                            lines.add(theContentsOfTheFile);
-                          });
-                        } else if (firstWord == 'mkfile') {
-                          if (wordsOfTheCommand[1].isNotEmpty)
-                            _makeFile(wordsOfTheCommand[1]);
-                          else
-                            setState(() {
-                              lines.add(
-                                  'Ugh, Don\'t forget to specify the file path!');
-                            });
-                        } else if (firstWord == 'mkdir') {
-                          String path =
-                              await _makeDirectory(wordsOfTheCommand[1]);
-                          setState(() {
-                            lines.add(path);
-                          });
-                        } else if (firstWord == 'cd') {
-                          // lines.add(Directory.current.path);
-                          Directory.current = wordsOfTheCommand[1];
-                          lines.add(Directory.current.path);
-                        } else {
-                          lines.add('This command hasn\'t been added yet!');
-                        }
-                      }
-                      controller.clear();
+                      await handleCommandSubmission();
                     },
                   ),
                 ),
@@ -158,16 +108,61 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Future<String> _readAFile(String path) async {
-    if (path.isEmpty) return 'Ugh, Don\'t forget to specify the file path!';
-    var lines = StringBuffer();
-    await File(path).readAsString().then((value) => lines.write(value));
-    // print('heeeey: $lines');
-    return lines.toString();
+  Future handleCommandSubmission() async {
+    setState(() {
+      lines.add(Command(text: controller.text));
+    });
+    //execute system available commands with no args
+    final process = await Process.run(controller.text.toLowerCase(), [])
+        .catchError((error) => print(error));
+    final envVarMap = Platform.environment;
+    if (process != null || envVarMap[controller.text.toUpperCase()] != null) {
+      setState(() {
+        lines.add(Output(
+            text: process?.stdout ?? envVarMap[controller.text.toUpperCase()]));
+      });
+    } else {
+      await maybeExecuteBuiltInCommand();
+    }
+    _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    controller.clear();
   }
 
-  Future<void> _makeFile(String path) async {
-    File(path).create(recursive: true);
+  Text buildShellText(String componentText) {
+    return Text(
+      componentText,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Future maybeExecuteBuiltInCommand() async {
+    List<String> wordsOfTheCommand = controller.text.split(' ');
+    String firstWord = wordsOfTheCommand[0];
+    if (firstWord == 'cat') {
+      await cat(wordsOfTheCommand);
+    } else if (firstWord == 'touch') {
+      touch(wordsOfTheCommand);
+    } else if (firstWord == 'mkdir') {
+      await mkdir(wordsOfTheCommand);
+    } else if (firstWord == 'cd') {
+      cd(wordsOfTheCommand);
+    } else {
+      lines.add(Output(text: 'This command hasn\'t been added yet!'));
+    }
+  }
+
+  void cd(List<String> wordsOfTheCommand) {
+    Directory.current = wordsOfTheCommand[1];
+    lines.add(Output(text: Directory.current.path));
+  }
+
+  Future mkdir(List<String> wordsOfTheCommand) async {
+    String path = await _makeDirectory(wordsOfTheCommand[1]);
+    setState(() {
+      lines.add(Output(text: path));
+    });
   }
 
   Future<String> _makeDirectory(String folderName) async {
@@ -176,13 +171,57 @@ class _HomePageState extends State<HomePage> {
 
     final Directory _dir = await getApplicationDocumentsDirectory();
     final Directory _tryCreatingNewFolder =
-        Directory('${_dir.path}/$folderName/');
+    Directory('${_dir.path}/$folderName/');
     if (await _tryCreatingNewFolder.exists())
       return _tryCreatingNewFolder.path;
     else {
       final Directory _newFolder =
-          await _tryCreatingNewFolder.create(recursive: true);
+      await _tryCreatingNewFolder.create(recursive: true);
       return _newFolder.path;
     }
+  }
+
+  void touch(List<String> wordsOfTheCommand) {
+    if (wordsOfTheCommand[1].isNotEmpty)
+      _makeFile(wordsOfTheCommand[1]);
+    else
+      setState(() {
+        lines.add(Output(text: 'Ugh, Don\'t forget to specify the file path!'));
+      });
+  }
+
+  Future<void> _makeFile(String path) async {
+    File(path).create(recursive: true);
+  }
+
+  Future cat(List<String> wordsOfTheCommand) async {
+    var theContentsOfTheFile = await _readAFile(wordsOfTheCommand[1] ?? '');
+    setState(() {
+      lines.add(Output(text: theContentsOfTheFile));
+    });
+  }
+
+  Future<String> _readAFile(String path) async {
+    if (path.isEmpty) return 'Ugh, Don\'t forget to specify the file path!';
+    var lines = StringBuffer();
+    await File(path).readAsString().then((value) => lines.write(value));
+    return lines.toString();
+  }
+
+
+
+  Widget buildShellComponent(ShellComponent shellComponent) {
+    if (shellComponent is Command) {
+      return Row(
+        children: [
+          Text(
+            "~\$ ",
+            style: TextStyle(color: Colors.green),
+          ),
+          buildShellText(shellComponent.text)
+        ],
+      );
+    }
+    return buildShellText((shellComponent as Output).text);
   }
 }
